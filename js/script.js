c658326f77d3b3862632674aa85266e7698775be
zac <isaacmubiru99@gmail.com>
2021-07-24 21:10:57 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -1,86 +1,57 @@
-// Slider
-const slider = function () {
-    const slides = document.querySelectorAll('.slide');
-    const btnLeft = document.querySelector('.slider__btn--left');
-    const btnRight = document.querySelector('.slider__btn--right');
-    const dotContainer = document.querySelector('.dots');
+//tas
+const tabs = document.querySelectorAll(".btn__tab");
+const tabsContainer = document.querySelector(".objectives__tabs-container");
+const tabsContent = document.querySelectorAll(".objectives__content");
+const nav = document.querySelector(".nav");
+const navHeight = nav.getBoundingClientRect().height;
+const header = document.querySelector(".header");
+const services = document.querySelector(".services");
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".btn__tab");
+  console.log("clicked", clicked);
+  if (!clicked) return;
+  tabs.forEach((t) => t.classList.remove("btn__tab--active"));
+  clicked.classList.add("btn__tab--active");
+  tabsContent.forEach((c) => c.classList.remove("objectives__content--active"));
+  document
+    .querySelector(`.objectives__content--${clicked.dataset.tab}`)
+    .classList.add("objectives__content--active");
+});
 
-  console.log("from js nigga")
-    let curSlide = 0;
-    const maxSlide = slides.length;
-  
-    // Functions
-    const createDots = function () {
-      slides.forEach(function (_, i) {
-        dotContainer.insertAdjacentHTML(
-          'beforeend',
-          `<button class="dots__dot" data-slide="${i}"></button>`
-        );
-      });
-      console.log("from create dots",dotContainer)
-    };
-  
-    const activateDot = function (slide) {
-      document
-        .querySelectorAll('.dots__dot')
-        .forEach(dot => dot.classList.remove('dots__dot--active'));
-  
-      document
-        .querySelector(`.dots__dot[data-slide="${slide}"]`)
-        .classList.add('dots__dot--active');
-    };
-  
-    const goToSlide = function (slide) {
-      slides.forEach(
-        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-      );
-    };
-  
-    // Next slide
-    const nextSlide = function () {
-      if (curSlide === maxSlide - 1) {
-        curSlide = 0;
-      } else {
-        curSlide++;
-      }
-      console.log("btn clicked")
-      goToSlide(curSlide);
-    //   activateDot(curSlide);
-    };
-  
-    const prevSlide = function () {
-      if (curSlide === 0) {
-        curSlide = maxSlide - 1;
-      } else {
-        curSlide--;
-      }
-      goToSlide(curSlide);
-    //   activateDot(curSlide);
-    };
-  
-    const init = function () {
-      goToSlide(0);
-    //   createDots();
-  
-    //   activateDot(0);
-    };
-    init();
-  
-    // Event handlers
-    btnRight.addEventListener('click', nextSlide);
-    btnLeft.addEventListener('click', prevSlide);
-  
-    document.addEventListener('keydown', function (e) {
-      if (e.key === 'ArrowLeft') prevSlide();
-      e.key === 'ArrowRight' && nextSlide();
-    });
-  
-    dotContainer.addEventListener('click', function (e) {
-      if (e.target.classList.contains('dots__dot')) {
-        const { slide } = e.target.dataset;
-        goToSlide(slide);
-        activateDot(slide);
-      }
-    });
-  };
-  slider();
+//stick navigation
+window.addEventListener("scroll", (e) => {});
+
+const obsCallback = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const headerObserver = new IntersectionObserver(obsCallback, obsOptions);
+
+headerObserver.observe(header);
+
+const allSections = document.querySelectorAll(".section");
+
+//reveal
+console.log("sections are", allSections);
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
